940fb8670ecf22368fc1f354fd67021e82b8a082
Duc Tran <duc.tranthuan@gmail.com>
2022-01-20 12:29:44 -0500

    fix web3 not injected by metamask anymore

--- src/app.js
+++ src/app.js
@@ -11,30 +11,23 @@ App = {
 
   // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
   loadWeb3: async () => {
-    if (typeof web3 !== 'undefined') {
-      App.web3Provider = web3.currentProvider
-      web3 = new Web3(web3.currentProvider)
-    } else {
-      window.alert("Please connect to Metamask.")
-    }
     // Modern dapp browsers...
     if (window.ethereum) {
-      window.web3 = new Web3(ethereum)
+      window.web3 = new Web3(ethereum);
       try {
-        // Request account access if needed
-        await ethereum.enable()
-        // Acccounts now exposed
-        web3.eth.sendTransaction({/* ... */})
+          // Request account access if needed
+          await ethereum.enable();
+          // Acccounts now exposed
+          web3.eth.sendTransaction({/* ... */});
       } catch (error) {
-        // User denied account access...
+          // User denied account access...
       }
     }
     // Legacy dapp browsers...
     else if (window.web3) {
-      App.web3Provider = web3.currentProvider
-      window.web3 = new Web3(web3.currentProvider)
+      window.web3 = new Web3(web3.currentProvider);
       // Acccounts always exposed
-      web3.eth.sendTransaction({/* ... */})
+      web3.eth.sendTransaction({/* ... */});
     }
     // Non-dapp browsers...
     else {
@@ -45,6 +38,8 @@ App = {
   loadAccount: async () => {
     // Set the current blockchain account
     App.account = web3.eth.accounts[0]
+    console.log(App.account)
+    console.log("test")
   },
 
   loadContract: async () => {
